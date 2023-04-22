4156f987bc18b2897b80aa683c0df18cbc5f3cfa
Ruslan Pashayev <pashayevrn@gmail.com>
2023-04-21 19:36:35 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, session
+from random import randint
+
+app = Flask(__name__)
+app.secret_key = '123456'
+
+@app.route('/')
+def start():
+    if 'num' not in session:
+        session['num'] =  randint(1, 100)
+    return render_template('game.html')
+
+@app.route('/guess', methods = ['POST'])
+def check_number():
+    session['guess'] = int(request.form['guess_number'])
+    print(session)
+    return redirect('/')
+
+@app.route('/reset', methods = ['POST'])
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug = True)
